251970c871ac2473b46fce240ba2aa342ec9b913
unknown <qian.hai@gmail.com>
2018-05-24 01:15:23 -0400

    working on p2p websocket

--- app/index.js
+++ app/index.js
@@ -1,15 +1,28 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
+const P2pServer = require('./p2p-server');
 
-const HTTP_PORT = process.env.HTTP_PORT || 80;
+const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new Blockchain();
+const p2pServer = new P2pServer(bc);
+
+app.use(bodyParser.json());
 
 app.get('/blocks', function(req, res) {
     res.json(bc.chain);
 });
 
+app.post('/mine', function(req, res) {
+    const block = bc.addBlock(req.body.data);
+    console.log(`New block added: ${block.toString()}`);
+    
+    res.redirect('/blocks');
+});
+
 app.listen(HTTP_PORT, function() {
     console.log(`Listening on port ${HTTP_PORT}`);
 });
+p2pServer.listen();
