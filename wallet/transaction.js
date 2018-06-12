f872e497d3ce52ae5de0a06808e2768f5457e8d8
unknown <qian.hai@gmail.com>
2018-06-11 23:48:45 -0400

    working on transaction pool

--- wallet/transaction.js
+++ wallet/transaction.js
@@ -8,6 +8,21 @@ class Transaction {
         this.outputs = [];
     }
     
+    update(senderWallet, recipient, amount) {
+        const senderOutput = this.outputs.find(output => output.address === senderWallet.publicKey);
+        
+        if (amount > senderOutput.amount) {
+            console.log(`Amount: ${amount} exceeds balabce.`);
+            return;
+        }
+        
+        senderOutput.amount = senderOutput.amount - amount;
+        this.outputs.push({amount: amount, address: recipient });
+        Transaction.signTransaction(this, senderWallet);
+        
+        return this;
+    }
+    
     static newTransaction(senderWallet, recipient, amount) {
         const transaction = new this();
         
@@ -32,6 +47,14 @@ class Transaction {
             address: senderWallet.publicKey,
             signature: senderWallet.sign(ChainUtil.hash(transaction.outputs))
         }
+    }
+    
+    static verifyTransaction(transaction) {
+        return ChainUtil.verifySignature(
+            transaction.input.address,
+            transaction.input.signature,
+            ChainUtil.hash(transaction.outputs)
+        );
     }
 }
 
